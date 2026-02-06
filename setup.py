ac46e1a9ed7be798608a3f2fb127b6f99773a0fd
pengw00 <water685@hotmail.com>
2026-02-05 22:51:00 -0800

    update nvcc for T4

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
             sources=['csrc/flash_att.cu', 'csrc/binding.cpp'],
             extra_compile_args={
                 'cxx': ['-O3'],
-                'nvcc': ['-O3', '--use_fast_math', '-gencode', 'arch=compute_80,code=sm_80']
+                # 'nvcc': ['-O3', '--use_fast_math', '-gencode', 'arch=compute_80,code=sm_80']
+                'nvcc': ['-O3', '--use_fast_math', '-gencode', 'arch=compute_75,code=sm_75']
+
             }
         )
     ],
